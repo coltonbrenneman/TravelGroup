533b634195d4cff8ab6140d405eb6936623cf15f
Colton Brenneman <colton.brenneman@gmail.com>
2023-08-16 14:01:33 -0600

    initial commit

---
+++ TravelGroup/TravelGroup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelGroup
+//
+//  Created by Colton Brenneman on 8/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
