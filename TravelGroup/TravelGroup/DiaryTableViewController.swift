ef5254e7e207a376e4994ef2bf0b5d4f7e2eda7e
Briana Bayne <brianabayne@brianas-imac.lan>
2023-08-16 15:13:47 -0600

    UI for tableview and custom cell, tableview file.

---
+++ TravelGroup/TravelGroup/DiaryTableViewController.swift
@@ -0,0 +1,62 @@
+//
+//  DiaryTableViewController.swift
+//  TravelGroup
+//
+//  Created by Briana Bayne on 8/16/23.
+//
+
+import UIKit
+
+class DiaryTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.reloadData()
+        
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return LogController.shared.logs.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "brianaIsAmazing", for: indexPath)
+            
+        let log = LogController.shared.logs[indexPath.row]
+        var config = cell.defaultContentConfiguration()
+        config.text = log.logTitle
+        config.secondaryText = log.logDate.asString()
+        cell.contentConfiguration = config
+        
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let dairy = LogController.shared.logs[indexPath.row]
+            LogController.shared.delete(log: dairy)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+  
+  
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailVC" {
+            if let indexPath == tableView.indexPathForSelectedRow {
+                if let destination = segue.destination as? LogDetailViewController {
+                    let dairy = LogController.shared.logs[indexPath.row]
+                    destination.log = dairy
+                }
+            }
+        }
+    }
+ 
+
+}
